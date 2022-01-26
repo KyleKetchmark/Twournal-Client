f6b59ceff40a107101d6541899b38cd2b0fc667f
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2022-01-25 22:35:54 -0800

    final

--- twournal/src/components/Auth/Login.jsx
+++ twournal/src/components/Auth/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, FormGroup, Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import './login.css';
 
 class Login extends Component {
